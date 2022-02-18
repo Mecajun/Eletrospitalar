4ad5e34f458e4980d065a37975164d28bf2636e6
clarafortes12 <clarafortes12@gmail.com>
2022-02-18 18:35:24 -0300

    Código para poder testar leitura

--- Testes/BLE_central/BLE_central.ino
+++ Testes/BLE_central/BLE_central.ino
@@ -49,10 +49,20 @@ void loop() {
         // print the advertised service UUIDs, if present
         if (peripheral.hasAdvertisedServiceUuid()) {
           Serial.print("Service UUIDs: ");
-          for (int i = 0; i < peripheral.advertisedServiceUuidCount(); i++) {
-            Serial.print(peripheral.advertisedServiceUuid(i));
-            Serial.print(" ");
-          }
+            // loop the services of the peripheral and explore each
+          for (int i = 0; i < peripheral.serviceCount(); i++) {
+            BLEService service = peripheral.service(i);
+        
+            for (int i = 0; i < service.characteristicCount(); i++) {
+              BLECharacteristic characteristic = service.characteristic(i);
+          
+              if (characteristic.valueLength() > 0) {
+                // print out the value of the characteristic
+                Serial.print(", value 0x");
+                printData(characteristic.value(), characteristic.valueLength());
+              }
+            }
+           }
           Serial.println();
         }
         
